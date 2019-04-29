b7f4a1a8ac7c4f3a7af691d04fe422503d77cca2
David Cohoe <sudgylacmoe@gmail.com>
2019-04-29 10:45:25 -0700

    Added descriptions of the packages

---
+++ src/main/java/edu/pdx/imagej/fringe_visibility/package-info.java
@@ -0,0 +1,5 @@
+/** The fringe visibility package includes all of the algorithms and classes
+ * for calculating the fringe visibility of a hologram.  It is still included
+ * in the phase unwrapping plugin because it is not used anywhere else.
+ */
+package edu.pdx.imagej.fringe_visibility;
---
+++ src/main/java/edu/pdx/imagej/phase_unwrapping/package-info.java
@@ -0,0 +1,4 @@
+/** The phase unwrapping package includes all of the algorithms and classes
+ * devoted to phase unwrapping.
+ */
+package edu.pdx.imagej.phase_unwrapping;
