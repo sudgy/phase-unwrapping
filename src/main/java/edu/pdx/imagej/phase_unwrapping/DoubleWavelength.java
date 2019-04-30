59a53abf030dd62c80421cd15487ed287c2a6665
David Cohoe <sudgylacmoe@gmail.com>
2019-04-30 09:44:55 -0700

    Barely split double wavelength into Op and Command

--- src/main/java/edu/pdx/imagej/phase_unwrapping/DoubleWavelength.java
+++ src/main/java/edu/pdx/imagej/phase_unwrapping/DoubleWavelength.java
@@ -26,6 +26,7 @@ import ij.process.FloatProcessor;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import net.imagej.ops.OpService;
 
 @Plugin(type = Command.class, menuPath = "Plugins>DHM>Phase Unwrapping>Double Wavelength")
 public class DoubleWavelength implements Command {
@@ -39,24 +40,66 @@ public class DoubleWavelength implements Command {
     private int M_wavelength2;
     @Parameter(label = "Pixel phase value")
     private float M_phase = 6.2831853072f;
-    //@Parameter(label = "Debug")
+    @Parameter(label = "Debug")
     private boolean M_debug = false;
+    @Parameter OpService P_ops;
 
     private float M_combined_wavelength;
 
     @Override
     public void run() {
-        M_combined_wavelength = (float)(M_wavelength1 * M_wavelength2) / Math.abs(M_wavelength1 - M_wavelength2);
-        ImageStack coarse_stack = new ImageStack(M_phase_image1.getWidth(), M_phase_image1.getHeight());
-        ImageStack fine_stack = new ImageStack(M_phase_image1.getWidth(), M_phase_image1.getHeight());
-        ImageStack debug_stack = null;
-        if (M_debug) debug_stack = new ImageStack(M_phase_image1.getWidth(), M_phase_image1.getHeight());
-
-        int final_size = Math.min(M_phase_image1.getImageStackSize(), M_phase_image2.getImageStackSize());
-        for (int i = 0; i < final_size; ++i) {
-            if (final_size > 1) {
-                IJ.showProgress(i + 1, final_size);
+        int width = M_phase_image1.getWidth();
+        int height = M_phase_image1.getHeight();
+        ImageStack coarse_stack = new ImageStack(width, height);
+        ImageStack fine_stack = new ImageStack(width, height);
+        ImageStack[] debug_stack = null;
+        if (M_debug) {
+            debug_stack = new ImageStack[7];
+            for (int i = 0; i < 7; ++i) {
+                debug_stack[i] = new ImageStack(width, height);
             }
+        }
+
+        int t_size = Math.min(M_phase_image1.getNFrames(), M_phase_image2.getNFrames());
+        int z_size = Math.min(M_phase_image1.getNSlices(), M_phase_image2.getNSlices());
+        int final_size = t_size * z_size;
+        int i = 0;
+        for (int t = 1; t <= t_size; ++t) {
+            for (int z = 1; z <= z_size; ++z) {
+                if (final_size > 1) {
+                    IJ.showProgress(i + 1, final_size);
+                    ++i;
+                }
+                int current_slice1 = M_phase_image1.getStackIndex(1, z, t);
+                int current_slice2 = M_phase_image2.getStackIndex(1, z, t);
+                float[][] img1 = M_phase_image1.getStack().getProcessor(current_slice1).getFloatArray();
+                float[][] img2 = M_phase_image2.getStack().getProcessor(current_slice2).getFloatArray();
+                DoubleWavelengthOp.PhaseImage image1 =
+                                            new DoubleWavelengthOp.PhaseImage();
+                image1.phase_image = img1;
+                image1.wavelength = M_wavelength1;
+                image1.phase_value = M_phase;
+                DoubleWavelengthOp.PhaseImage image2 =
+                                            new DoubleWavelengthOp.PhaseImage();
+                image2.phase_image = img2;
+                image2.wavelength = M_wavelength2;
+                image2.phase_value = M_phase;
+                float[][][] result = (float[][][])P_ops.run("Double Wavelength Phase Unwrapping", image1, image2, M_debug);
+                if (M_debug) {
+                    coarse_stack.addSlice(new FloatProcessor(result[3]));
+                    fine_stack.addSlice(new FloatProcessor(result[6]));
+                    for (int j = 0; j <= 6; ++j) {
+                        debug_stack[j].addSlice(new FloatProcessor(result[j]));
+                    }
+                }
+                else {
+                    coarse_stack.addSlice(new FloatProcessor(result[0]));
+                    fine_stack.addSlice(new FloatProcessor(result[1]));
+                }
+            }
+        }
+            /*
+        for (int i = 0; i < final_size; ++i) {
             boolean debug = M_debug && i == 0;
             float[][] img1 = M_phase_image1.getStack().getProcessor(i + 1).getFloatArray();
                 if (debug) debug_stack.addSlice("Phase Image 1 (a)", new FloatProcessor(img1));
@@ -82,7 +125,25 @@ public class DoubleWavelength implements Command {
             fine_stack.addSlice(new FloatProcessor(result));
             if (debug) (new ImagePlus("Test", debug_stack)).show();
         }
-        (new ImagePlus("Coarse Map", coarse_stack)).show();
-        (new ImagePlus("Fine Map", fine_stack)).show();
+            */
+        if (M_debug) {
+            show_stack(debug_stack[0], "Phase Image 1 (a)", z_size, t_size);
+            show_stack(debug_stack[1], "Phase Image 2 (b)", z_size, t_size);
+            show_stack(debug_stack[2], "Phase Difference (c)", z_size, t_size);
+            show_stack(debug_stack[3], "Coarse Map (d)", z_size, t_size);
+            show_stack(debug_stack[4], "Round to Phase 1 (e)", z_size, t_size);
+            show_stack(debug_stack[5], "Round + Phase 1 (f)", z_size, t_size);
+            show_stack(debug_stack[6], "Fine Map (g)", z_size, t_size);
+        }
+        else {
+            show_stack(coarse_stack, "Coarse Map", z_size, t_size);
+            show_stack(fine_stack, "Fine Map", z_size, t_size);
+        }
+    }
+    private void show_stack(ImageStack stack, String label, int z_size, int t_size)
+    {
+        ImagePlus imp = IJ.createHyperStack(label, stack.getWidth(), stack.getHeight(), 1, z_size, t_size, 32);
+        imp.setStack(stack);
+        imp.show();
     }
 }
---
+++ src/main/java/edu/pdx/imagej/phase_unwrapping/DoubleWavelengthOp.java
@@ -0,0 +1,100 @@
+/* Copyright (C) 2019 Portland State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For any questions regarding the license, please contact the Free Software
+ * Foundation.  For any other questions regarding this program, please contact
+ * David Cohoe at dcohoe@pdx.edu.
+ */
+
+package edu.pdx.imagej.phase_unwrapping;
+
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import net.imagej.ops.AbstractOp;
+import net.imagej.ops.Op;
+
+@Plugin(type = Op.class, name = "Double Wavelength Phase Unwrapping")
+public class DoubleWavelengthOp extends AbstractOp {
+    static public class PhaseImage {
+        public float[][] phase_image;
+        public float wavelength;
+        public float phase_value;
+    }
+    // Inputs
+    @Parameter private PhaseImage P_image1;
+    @Parameter private PhaseImage P_image2;
+    @Parameter private boolean P_show_steps;
+    // Outputs
+    @Parameter(type = ItemIO.OUTPUT) float[][][] P_result;
+
+    @Override
+    public void run()
+    {
+        float combined_wavelength =
+                    (P_image1.wavelength * P_image2.wavelength) /
+            Math.abs(P_image1.wavelength - P_image2.wavelength);
+        int width = P_image1.phase_image.length;
+        int height = P_image1.phase_image[0].length;
+        float phase_value = P_image1.phase_value;
+        if (P_image1.phase_value != P_image2.phase_value) {
+            for (int x = 0; x < width; ++x) {
+                for (int y = 0; y < height; ++y) {
+                    P_image2.phase_image[x][y] *=
+                        (phase_value / P_image2.phase_value);
+                }
+            }
+        }
+        float[][][] result = new float[7][width][height];
+        result[0] = P_image1.phase_image;
+        result[1] = P_image2.phase_image;
+        result[2] = subtract_images(P_image1.phase_image, P_image2.phase_image);
+        result[3] = create_coarse_from_difference(result[2], phase_value,
+                                      combined_wavelength, P_image1.wavelength);
+        result[4] = round_to(result[3], phase_value);
+        result[5] = add_images(result[4], P_image1.phase_image);
+        result[6] = bring_close_to(result[5], result[3], phase_value);
+        if (P_show_steps) P_result = result;
+        else P_result = new float[][][] {result[3], result[6]};
+    }
+    static private float[][] subtract_images(float[][] image1, float[][] image2)
+    {
+        return ArrayOps.binary(image1, image2, ArrayOps.Subtract);
+    }
+    static private float[][] create_coarse_from_difference(
+        float[][] difference, float phase_value,
+        float combined_wavelength, float wavelength1)
+    {
+        return ArrayOps.unary(
+            ArrayOps.unary(difference, a -> a < 0 ? a + phase_value : a),
+            ArrayOps.MultiplyBy(combined_wavelength / wavelength1));
+    }
+    static private float[][] round_to(float[][] image, float round)
+    {
+        return ArrayOps.unary(image, a -> (int)(a / round) * round);
+    }
+    static private float[][] add_images(float[][] image1, float[][] image2)
+    {
+        return ArrayOps.binary(image1, image2, ArrayOps.Add);
+    }
+    static private float[][] bring_close_to(float[][] from,
+                                            float[][] to,
+                                            float threshold)
+    {
+        return ArrayOps.binary(from, to,
+            (a, b) -> Math.abs(a - b) > (threshold / 2)
+                ? a - threshold * Math.signum(a - b)
+                : a);
+    }
+}
